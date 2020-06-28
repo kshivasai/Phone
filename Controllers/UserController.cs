8fb1a16119a271cb4106b9e57aa42c8e2b23a31c
kshivasai <kshivasai@pkglobal.com>
2020-06-28 18:25:50 +0530

    Initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Phone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.CompilerServices;
+using Phone.Models;
+using System.Collections;
+
+namespace Phone.Controllers
+{
+    public class UserController : Controller
+    {
+       
+        // GET: User
+        public ActionResult Dashboard()
+        { 
+            return View();
+        }
+        public ActionResult Enter()
+        {
+            return View("EnterUser");
+        }   
+        public ActionResult Submit(UserData data)
+        {
+           
+         
+            //UserData userData = new UserData();
+            //userData.Name = Request.Form["Name"];
+            //userData.Address_line_1 = Request.Form["Address_line_1"];
+            //userData.Address_line_2 = Request.Form["Address_line_2"];
+            //userData.City = Request.Form["City"];
+            //userData.Pincode = Request.Form["Pincode"];
+            //userData.Phonenumber = Request.Form["Phonenumber"];
+
+            return View(data);
+        }
+      
+    }
+
+}
---
+++ Models/UserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phone.Models
+{
+    public class UserData
+    {
+       
+        public string Name { get; set; }
+        public String Address_line_1 { get; set; }
+        public string Address_line_2 { get; set; }
+        public string City { get; set; }
+        public string Pincode { get; set; }
+        public string Phonenumber { get; set; }
+
+    }
+}
